8f4c5358a12cf4e33e01b4fe1646a53119a12b87
Nate Day <55556056+s445276@users.noreply.github.com>
2021-03-26 13:09:31 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+hungryness = 0
+
+def on_forever():
+    global hungryness
+    if input.button_is_pressed(Button.A):
+        hungryness = hungryness + 1
+        basic.show_number(hungryness)
+    elif input.button_is_pressed(Button.B):
+        hungryness = hungryness - 1
+        basic.show_number(hungryness)
+basic.forever(on_forever)
+
